27137f1822c17951141091fef5692d53e6f32e9c
Daniel Goldberg <danpaulgo@aol.com>
2017-04-26 22:25:31 -0400

    Complete user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
+  resources :users
+  post 'signup', to: 'users#create'
+  get 'signup', to: 'users#new'
 
   root 'static_pages#home'
   get 'home', to: 'static_pages#home'
   get 'about', to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
-  get 'signup', to: 'users#new'
   get 'help',  to: 'static_pages#help', as: "helping"
   # SAME AS
   # get("help", {to: "static_pages#contact"})
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+  test "invalid signup submission" do
+    assert_no_difference 'User.count' do
+      post signup_path, params:{user:{name: "", email: "user@invaldid", password: "foo", password_confirmation: "bar"}}
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+  end
+
+  test "valid signup submission" do
+    assert_difference 'User.count' do
+      post signup_path, params:{user:{name: "Example User", email: "user@example.com", password: "password", password_confirmation: "password"}}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'div.alert'
+    assert_not flash.empty?
+  end
+
+end
